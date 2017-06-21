9943fff252a0588b093b627da7cb4fae52e0673d
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-06-21 17:40:26 +0300

    Pass tests

--- bucky/models.py
+++ bucky/models.py
@@ -1,10 +1,18 @@
 class User():
-    pass
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
 class BucketList():
-    pass
+    def __init__(self, title, description, owner):
+        self.title = title
+        self.description = description
+        self.owner = owner
 
 
 class BucketListItem():
-    pass
+    def __init__(self, name, bucket, achieved=False):
+        self.name = name
+        self.bucket = bucket
+        self.achieved = achieved
