059609d093aec4e9a6bb6c957b0f06af8d193089
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-06-21 18:27:02 +0300

    Remove unused imports

--- bucky/forms.py
+++ bucky/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Email, username
+from wtforms import StringField, PasswordField
+from wtforms.validators import Required, username
 
 
 class LoginForm(Form):
