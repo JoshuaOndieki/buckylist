7292cc78b60ec58ce4fdfea48c3f0460afc1318f
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-06-21 17:40:13 +0300

    Refactor tests

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -5,5 +5,5 @@ class TestBucketList(unittest.TestCase):
     def test_creates_bucketlist(self):
         test_bucket = BucketList("2017 bucket", "2017 description", 'Oj')
         self.assertEqual(test_bucket.title, '2017 bucket')
-        self.assertEqual(self.description, '2017 description')
-        self.assertEqual(self.owner, 'Oj')
+        self.assertEqual(test_bucket.description, '2017 description')
+        self.assertEqual(test_bucket.owner, 'Oj')
