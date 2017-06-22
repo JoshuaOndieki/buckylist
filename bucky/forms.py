dc8d16f26eda757d11efe20c6394901b7212e5a9
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-06-22 15:29:16 +0300

    Use FlaskForm for forms

--- bucky/forms.py
+++ bucky/forms.py
@@ -3,11 +3,11 @@ from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
 
 
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
