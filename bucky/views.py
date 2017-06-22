3e304f81dd7dfe8d7141ebee4793406eeab1ae85
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-06-22 15:31:57 +0300

    Create working login view

--- bucky/views.py
+++ bucky/views.py
@@ -2,33 +2,43 @@ from flask import Blueprint
 
 views = Blueprint('views', __name__)
 
-from flask_login import current_user
+# from flask_login import current_user
 from run import app
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user
+from flask_login import login_user, login_required, logout_user
 from .models import User, BucketList, BucketListItem
 from .forms import LoginForm, RegistrationForm
 
 
 @views.route('/', methods = ['GET'])
+@login_required
 def index():
-    if current_user.is_authenticated:
-        return render_template ('index.html')
-    return render_template ('login.html')
+    """
+    Usage: Redirects unauthorised users to login and authorised users to index
+    :return:
+    """
+    return render_template ('index.html')
 
 @views.route('/login', methods=['GET','POST'])
 def login():
+    """
+    Usage: logs a user in the app with correct credentials
+    :return:
+    """
     if request.method == 'POST':
-        form = LoginForm(request.form, csrf_token = False)
-        if form.validate():
-            user = form.username
-            if user in app.database.keys() and form.password.data == app.database[user]['password']:
-                print("logging")
-                login_user(user)
-                return redirect('/')
+        print("its a post")
+        form = LoginForm(csrf_token = False)
+        if True:  # use form.validate_on_submit() !!!
+            for user in app.database:
+                print('start login')
+                if form.username.data == user.username and form.password.data == user.password:
+                    print("logging")
+                    login_user(user)
+                    return redirect('/')
             flash('username or password did not match.')
             return redirect(url_for('views.login'))
         print("login unsuccessful")
+    print("its not a post")
     return render_template('login.html')
 
 
@@ -58,4 +68,19 @@ def register():
             print("user exists")
         print("user creation unsuccessful")
         flash('Registration unsuccessful!')
-    return render_template('signup.html') #c+lient is requesting for register page so render it
+    return render_template('signup.html') #c+lient is requesting for register page so render
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+
+
+
+
+@views.route('/test', methods = ['GET'])
+def test():
+    return render_template('index.html')
