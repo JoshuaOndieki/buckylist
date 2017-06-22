04a970ad0f9614eda13808227358a59fbe4752a2
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-06-22 12:30:16 +0300

    Add config

--- bucky/__init__.py
+++ bucky/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_login import LoginManager
+from config import config
 
 database = {}
 
-def create_app():
+
+def create_app(config_name):
     app = Flask(__name__)
     app.database = database
+    app.config.from_object(config[config_name])
     login_manager = LoginManager()
     login_manager.init_app(app)
     from . import views
