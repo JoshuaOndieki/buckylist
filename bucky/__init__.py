9e2903aa3b5590b17838d38852ba0aa86bbeeb17
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-06-22 15:26:20 +0300

    Install load_user

--- bucky/__init__.py
+++ bucky/__init__.py
@@ -3,14 +3,20 @@ from flask_login import LoginManager
 from config import config
 
 database = {}
-
+login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.database = database
     app.config.from_object(config[config_name])
-    login_manager = LoginManager()
     login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(username):
+        for user in app.database:
+            if user.username == username:
+                return user
+
     from . import views
     app.register_blueprint(views.views)
     return app
