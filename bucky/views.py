013784e3d9158ae884f422b9b7358a9b0879ad30
JoshuaOndieki <joshuaondieki8@gmail.com>
2017-06-23 07:03:12 +0300

    Remove flask-login extension

--- bucky/views.py
+++ bucky/views.py
@@ -2,22 +2,21 @@ from flask import Blueprint
 
 views = Blueprint('views', __name__)
 
-from flask_login import current_user
 from run import app
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
 from .models import User, BucketList, BucketListItem
 from .forms import LoginForm, RegistrationForm
-
+from .helpers import get_user
 
 @views.route('/', methods = ['GET'])
-@login_required
 def index():
     """
     Usage: Redirects unauthorised users to login and authorised users to index
     :return:
     """
-    return render_template ('index.html')
+    if app.current_user is None:
+        return redirect(url_for('views.login'))
+    return render_template('index.html', data={'user': app.current_user})
 
 @views.route('/login', methods=['GET','POST'])
 def login():
@@ -25,23 +24,20 @@ def login():
     Usage: logs a user in the app with correct credentials
     :return:
     """
+    errors = []
     if request.method == 'POST':
-        print("its a post")
-        form = LoginForm(csrf_token = False)
-        if True:  # use form.validate_on_submit() !!!
+        form = LoginForm()
+        if True:  # N/B:!!! use form.validate_on_submit() !!!
             for user in app.database:
-                print('start login')
                 if form.username.data == user.username and form.password.data == user.password:
-                    print("logging")
-                    login_user(user)
+                    app.current_user = user
                     return redirect('/')
-            flash('username or password did not match.')
-            return redirect(url_for('views.login'))
-        print("login unsuccessful")
-    print("its not a post")
-    if current_user:
-        return render_template('/')
-    return render_template('login.html')
+            errors = ['username or password did not match.']
+            print(errors)
+            return render_template('login.html', data={'errors': errors })
+    if app.current_user: # always send logged in users to panel with index view
+        return redirect('/')
+    return render_template('login.html', data={'errors': errors})
 
 
 
@@ -51,51 +47,26 @@ def register():
     Usage: Creates a new user in the app
     :return:
     """
+    errors = []
+    success = None
     if request.method == 'POST':
-        form = RegistrationForm(request.form, csrf_token = False)
-        if True:
-            if form.username.data not in app.database:
-                print("form valid, creating user")
+        form = RegistrationForm()
+        if True:  # N/B:!!! use form.validate_on_submit() !!!
+            user_exists = get_user(form.username.data, app.database)
+            if user_exists:
+                errors = ['username taken']
+                return render_template('signup.html', data={'errors': errors})
+            else:
                 user = User(form.username.data,
                             form.password.data)
                 #add user to database
-                app.database.setdefault(user, {})
-                app.database[user].setdefault('buckets' , {})
-                app.database[user]['buckets'].setdefault('detail', '')
-                app.database[user]['buckets'].setdefault('items', [])
-                print("user created")
-                flash('Registration successful! You can log in now.')
+                app.database[user] = {}
+                success = 'Registration successful! You can log in now.'
                 return redirect(url_for('views.index'))
-            print("user exists")
-        print("user creation unsuccessful")
-        flash('Registration unsuccessful!')
-    return render_template('signup.html')
-
-# @app.route("/logout")
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect('/')
+    return render_template('signup.html', data={'errors': errors, 'success': success})
 
 
-
-@views.route('/logout')
-@login_required
+@views.route('/logout', methods = ['GET', 'POST'])
 def logout():
-    logout_user()
-    # redirect to the login page
-    return redirect(url_for('views.login'))
-
-
-
-
-
-
-
-
-
-
-
-@views.route('/test', methods = ['GET'])
-def test():
-    return render_template('index.html')
+    app.current_user = None
+    return redirect(url_for('views.index'))
