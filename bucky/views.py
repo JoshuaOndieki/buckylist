31a3fe3c49dd9eedcb0b9ccc337a8e3d9b47a222
JoshuaOndieki <joshuaondieki8@gmail.com>
2017-06-23 10:48:52 +0300

    Create addbucket view

--- bucky/views.py
+++ bucky/views.py
@@ -6,7 +6,7 @@ from run import app
 from flask import render_template, redirect, request, url_for, flash
 from .models import User, BucketList, BucketListItem
 from .forms import LoginForm, RegistrationForm
-from .helpers import get_user
+from .helpers import get_user, get_bucket
 
 @views.route('/', methods = ['GET'])
 def index():
@@ -16,7 +16,11 @@ def index():
     """
     if app.current_user is None:
         return redirect(url_for('views.login'))
-    return render_template('index.html', data={'user': app.current_user})
+
+    user_db = app.database[app.current_user]
+
+    return render_template('index.html', data={'user': app.current_user, 'db': user_db})
+
 
 @views.route('/login', methods=['GET','POST'])
 def login():
@@ -70,3 +74,22 @@ def register():
 def logout():
     app.current_user = None
     return redirect(url_for('views.index'))
+
+views.route('/addbucket', methods = ['POST'])
+def addbucket():
+    form = BucketForm()
+    if app.current_user:
+        bucket = get_bucket(form.name.data, app.database, app.current_user)
+        if bucket:
+            errors  = ['BucketList already exists!']
+            return redirect('/')
+        bucket = BucketList(form.title.data, form.description.data, app.current_user.username)
+        # store bucket
+        app.database[app.current_user].setdefault(bucket, [])
+        # app.database[app.current_user][bucket] = []
+        return redirect(url_for('/'))
+
+
+@views.route('/updatebucket', methods= ['POST'])
+def updatebucket():
+    pass
